ed9346b13a65622993e3d1622cbd666c111a5b49
amykglen <glena@oregonstate.edu>
2021-03-02 22:17:29 -0800

    Jsonify response from API

--- api.py
+++ api.py
@@ -1,14 +1,15 @@
 # Thank you https://towardsdatascience.com/creating-restful-apis-using-flask-and-python-655bad51b24
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from badger import BadgerDB
 app = Flask(__name__)
 badger = BadgerDB(is_test=True)
 
 
 @app.route('/query/', methods=['POST'])
-def query():
-    json_query = request.json
-    return badger.answer_query(json_query)
+def run_query():
+    query = request.json
+    answer = badger.answer_query(query)
+    return jsonify(answer)
 
 
 if __name__ == '__main__':
