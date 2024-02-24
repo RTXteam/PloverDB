5609211b9ad84447833b7068b444bf7071d0fca3
Amy Glen <49423686+amykglen@users.noreply.github.com>
2024-02-24 11:47:24 -0800

    Don't use deprecated '--progress' flag

--- run.sh
+++ run.sh
@@ -5,8 +5,11 @@ image_name="${1:-myimage}"
 container_name="${2:-mycontainer}"
 docker_command="${3:-docker}"
 
+set +e  # Don't stop on error
 ${docker_command} stop ${container_name}
 ${docker_command} rm ${container_name}
 ${docker_command} image rm ${image_name}
-${docker_command} build --no-cache --progress=plain -t ${image_name} .
+set -e  # Stop on error
+
+${docker_command} build --no-cache -t ${image_name} .
 ${docker_command} run -d --name ${container_name} -p 9990:80 ${image_name}
