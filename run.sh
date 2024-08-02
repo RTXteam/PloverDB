5bd6e96a2f330424907d6f1de73d337b23c3df60
amykglen <glena@oregonstate.edu>
2024-08-01 17:30:30 -0700

    Configure Gunicorn with ssl cert

--- run.sh
+++ run.sh
@@ -12,4 +12,16 @@ ${docker_command} image rm ${image_name}
 set -e  # Stop on error
 
 ${docker_command} build -t ${image_name} .
-${docker_command} run -d --name ${container_name} -p 9990:80 -e GUNICORN_CMD_ARGS="--preload" -e WORKERS_PER_CORE="1" ${image_name}
+
+# Run the docker container; NOTE: the '--preload' flag makes Gunicorn workers *share* (vs. copy) the central index (yay)
+if [ ${image_name} == "myimage" ]
+then
+  # Skip configuring SSL cert if this is a test build  (TODO: use a dedicated 'test' build flag..)
+  ${docker_command} run -d --name ${container_name} -p 9990:80 -e GUNICORN_CMD_ARGS="--preload" ${image_name}
+else
+  # Ensure our SSL cert is current and load it into the container (on run)
+  sudo certbot renew
+  cert_file_path=/etc/letsencrypt/live/ctkp.rtx.ai/fullchain.pem
+  key_file_path=/etc/letsencrypt/live/ctkp.rtx.ai/privkey.pem
+  ${docker_command} run -v ${cert_file_path}:${cert_file_path} -v ${key_file_path}:${key_file_path} -d --name ${container_name} -p 9990:443 -e GUNICORN_CMD_ARGS="--preload --keyfile=${key_file_path} --certfile=${cert_file_path}" -e PORT=443 ${image_name}
+fi
