023f973aa33a8968f98f138e6e156ca79b4a7ab4
amykglen <glena@oregonstate.edu>
2025-03-01 13:07:32 -0800

    Remove docker command and skip ssl params from rebuild server

--- rebuild_main.py
+++ rebuild_main.py
@@ -54,12 +54,10 @@ def rebuild_app(body: dict, authenticated: bool = Depends(auth_request)):
         else:
             start = time.time()
             host_port = body.get("port", "9990")
-            docker_command = body.get("docker_command", "sudo docker")
             image_name = f"ploverimage{f'-{branch_name}' if branch_name else ''}"
             container_name = f"plovercontainer{f'-{branch_name}' if branch_name else ''}"
-            skip_ssl = body.get("skip_ssl", False)
             build_err = os.system(f"bash -x {SCRIPT_DIR}/run.sh -b {branch_name} -i {image_name} -c {container_name} "
-                                  f"-p {host_port} -d '{docker_command}' -s {skip_ssl}")
+                                  f"-p {host_port}")
             if build_err:
                 raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                                     detail="500 ERROR: Rebuild failed. Check logs on server.")
